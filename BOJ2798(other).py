198621a64d74a2e993e0ef777b85bf4bd81f74be
Wooryung Sung <wooryung1004@gmail.com>
2022-08-31 15:59:34 +0900

    배열, 완전탐색

---
+++ BOJ2798(other).py
@@ -0,0 +1,15 @@
+#단순 3중 반복문을 사용하여 풀이
+#순열을 직접 구현하는 경우
+
+N, M = map(int, input().split())
+cards = list(map(int, input().split()))
+
+result = 0
+for i in range(0, len(cards)):
+    for j in range(i+1, len(cards)):
+        for k in range(j+1, len(cards)):
+            sum = cards[i] + cards[j] + cards[k]
+            if sum <= M:
+                result = max(result, sum)
+                
+print(result)
---
+++ BOJ2798.py
@@ -0,0 +1,14 @@
+#조합을 사용하여 풀이
+from itertools import combinations
+
+N, M = map(int, input().split())
+cards = list(map(int, input().split()))
+
+candidates = list(combinations(cards, 3))
+max = 0
+for candidate in candidates:
+    sum = candidate[0] + candidate[1] + candidate[2]
+    if (sum <= M and sum > max):
+        max = sum
+        
+print(max)
